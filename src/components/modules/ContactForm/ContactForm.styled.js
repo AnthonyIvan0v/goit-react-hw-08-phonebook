3d29105baa7b3eb8aa951099876333705f22ff6c
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2023-03-18 12:11:49 +0200

    adds changes

---
+++ src/components/modules/ContactForm/ContactForm.styled.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import { Form } from 'formik';
+
+export const Title = styled.h1`
+  margin: 20px;
+`;
+
+export const FormAdd = styled(Form)`
+  height: 100vh;
+  float: left;
+  margin-right: 20px;
+  margin-left: 20px;
+`;
+
+export const Label = styled.label`
+  display: block;
+`;
+
+export const ErrorsMessage = styled.div`
+  margin-top: 5px;
+  color: red;
+`;
+
+export const Section = styled.div`
+  width: 320px;
+`;
